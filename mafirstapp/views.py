e13004ae96fc9f44a248242cec96a6dd4f3e9189
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2016-01-23 05:40:00 +0100

    adds index.html

--- mafirstapp/views.py
+++ mafirstapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Author
 
 
 def index(request):
-	return HttpResponse('mafirst app index')
+	author_list = Author.objects.order_by('-name')[:5]
+	template = loader.get_template('mafirstapp/index.html')
+	context = {
+		'author_list': author_list
+	}
+	return HttpResponse(template.render(context, request))
