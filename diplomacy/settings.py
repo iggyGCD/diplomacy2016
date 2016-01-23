bbdd4929f450bd2eb24d7ef6df5b66a63aaf2e29
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2016-01-23 02:41:43 +0100

    adds Author and Book model

--- diplomacy/settings.py
+++ diplomacy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mafirstapp.apps.MafirstappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mafirstapp/models.py
+++ mafirstapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+	name = models.CharField(max_length=20)
+
+class Book(models.Model):
+	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
